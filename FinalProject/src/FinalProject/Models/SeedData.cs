fef95b18fa63efe52d9071a335e434d1947f1cf9
ThinhNu <thinh.le.sdev@gmail.com>
2016-09-27 09:37:19 +0700

    Vc

--- FinalProject/src/FinalProject/Models/SeedData.cs
+++ FinalProject/src/FinalProject/Models/SeedData.cs
@@ -4,7 +4,7 @@ using FinalProject.Data;
 using System;
 using System.Linq;
 
-namespace MvcMovie.Models
+namespace FinalProject.Models
 {
     public static class SeedData
     {
@@ -20,11 +20,39 @@ namespace MvcMovie.Models
                 }
 
                 context.CashAccount.AddRange(
-                    new FinalProject.Models.CashAccount
+                    new CashAccount
                     {
-                        AccountDescription = "Test",
-                        BankName = "ThinhNu",
-                        BankAccountNumber = "123"
+                        AccountDescription = "ACB Bank",
+                        BankName = "ThinhLe",
+                        BankAccountNumber = "051.100.389.1938"
+                    },
+
+                    new CashAccount
+                    {
+                        AccountDescription = "OCEAN Bank",
+                        BankName = "DatNgo",
+                        BankAccountNumber = "751.130.319.4316"
+                    },
+
+                    new CashAccount
+                    {
+                        AccountDescription = "Viettin Bank",
+                        BankName = "BinhBo",
+                        BankAccountNumber = "321.130.568.1954"
+                    },
+
+                    new CashAccount
+                    {
+                        AccountDescription = "ACB Bank",
+                        BankName = "NguyenTruong",
+                        BankAccountNumber = "052.900.679.2422"
+                    },
+
+                    new CashAccount
+                    {
+                        AccountDescription = "Dong A Bank",
+                        BankName = "NgocQuy",
+                        BankAccountNumber = "751.120.389.351"
                     }
                 );
                 context.SaveChanges();
